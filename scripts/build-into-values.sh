413086207c4587b8b134327406464b064a8a0f4a
Matt Royal <mroyal@pivotal.io>
2020-07-01 22:57:42 -0700

    cf-for-k8s works using ccng image built by paketo buildpacks

--- scripts/build-into-values.sh
+++ scripts/build-into-values.sh
@@ -8,10 +8,12 @@ REPO_BASE_DIR="${SCRIPT_DIR}/.."
 # set defaults - any of these should be configrable from outside this script
 IMAGE_DESTINATION_CCNG="${IMAGE_DESTINATION_CCNG:-gcr.io/cf-capi-arya/dev-ccng}"
 IMAGE_DESTINATION_KPACK_WATCHER="${IMAGE_DESTINATION_KPACK_WATCHER:-gcr.io/cf-capi-arya/dev-kpack-watcher}"
+IMAGE_DESTINATION_CAPI_NGINX="${IMAGE_DESTINATION_CAPI_NGINX:-gcr.io/cf-capi-arya/dev-capi-nginx}"
 CF_FOR_K8s_DIR="${CF_FOR_K8s_DIR:-${HOME}/workspace/cf-for-k8s/}"
 CAPI_RELEASE_DIR="${CAPI_RELEASE_DIR:-${HOME}/workspace/capi-release/}"
 CCNG_DIR="${CAPI_RELEASE_DIR}src/cloud_controller_ng"
 CAPI_KPACK_WATCHER_DIR="${CAPI_KPACK_WATCHER_DIR:-${REPO_BASE_DIR}/src/capi-kpack-watcher}"
+CAPI_NGINX_DIR="${REPO_BASE_DIR}/dockerfiles/nginx"
 
 if [ -z "$1" ]
   then
@@ -24,8 +26,10 @@ KBLD_TMP="$(mktemp)"
 ytt -f "${REPO_BASE_DIR}/dev-templates/" \
     -v kbld.destination.ccng="${IMAGE_DESTINATION_CCNG}" \
     -v kbld.destination.capi_kpack_watcher="${IMAGE_DESTINATION_KPACK_WATCHER}" \
+    -v kbld.destination.capi_nginx="${IMAGE_DESTINATION_CAPI_NGINX}" \
     -v src_dirs.ccng="${CCNG_DIR}" \
     -v src_dirs.capi_kpack_watcher="${CAPI_KPACK_WATCHER_DIR}" \
+    -v src_dirs.capi_nginx="${CAPI_NGINX_DIR}" \
      > "${KBLD_TMP}"
 
 # build a new values file with kbld
